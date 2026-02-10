9e0b63209b8e0e14f80da6637ec17f28aec1daf6
Bayrem Sayeh <bayremsayeh98@gmail.com>
2026-02-10 01:38:17 +0100

    update for deploiment project

---
+++ build.sh
@@ -0,0 +1,12 @@
+# build.sh
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+python manage.py migrate
+
+# Créer un superutilisateur si nécessaire
+python create_superuser.py
